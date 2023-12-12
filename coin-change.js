24f6543385f35e6790d2615ac7cf8f6ee4506a29
ChrisJettoo <cjettoostudent@gmail.com>
2023-12-12 10:55:47 -0500

    daily hack hour

---
+++ coin-change.js
@@ -0,0 +1,50 @@
+/*
+322. Coin Change
+Medium
+
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+
+ */
+
+
+var coinChange = function(coins, amount) {
+    // edge cases
+    // sort array
+    //  return -1 if cannot make up money
+    // return 0 if the amount of 0
+    // take the highest amount of coins and subtract from the amount
+    // if the amount if still higher than the highest coin, do it again continully do it 
+    // once it is lower than that highest amount, slice off the highest amount and continually check to see until value is 0
+
+    let sortedCoins = [...coins].sort((a, b) => a - b);
+    let counter = 0;
+    
+    while(amount !== 0){
+        console.log(sortedCoins)
+
+      
+
+      if(sortedCoins[sortedCoins.length - 1] <= amount){
+        amount = amount - sortedCoins[sortedCoins.length - 1];
+        counter++;
+      }else{
+          sortedCoins.pop();
+      }
+
+      if(sortedCoins.length === 0){
+        break;
+      }
+    }
+
+    return counter;
+};
+
+console.log(coinChange([1,2,5], 11));
